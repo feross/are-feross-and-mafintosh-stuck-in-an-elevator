d5fc555c7dbef70450b044b5061c79a6cad90d4f
Feross Aboukhadijeh <feross@feross.org>
2015-08-05 13:07:38 +0200

    use module

---
+++ index.js
@@ -0,0 +1,2 @@
+var areFerossAndMafintoshStuckInAnElevator = require('are-feross-and-mafintosh-stuck-in-an-elevator')
+document.body.innerText = areFerossAndMafintoshStuckInAnElevator() ? 'YES' : 'NO'
